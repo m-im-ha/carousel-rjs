c5bc95826bec26e9d93aff1bda33d3cb51db553b
Imran <sherlockholmesad221@gmail.com>
2024-02-08 12:13:51 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-const images = ["images/img-1.jpg",
-"images/img-2.jpg","images/img-3.jpg","images/img-4.jpg"];
+import { useState } from "react";
 
-export default function App(){
+const images = [
+  "images/img-1.jpg",
+  "images/img-2.jpg",
+  "images/img-3.jpg",
+  "images/img-4.jpg",
+];
+
+export default function App() {
+  const [currentImg, setCurentImg] = useState(0);
+
+  function prevBtn() {
+    setCurentImg(currentImg < 1 ? images.length - 1 : currentImg - 1);
+  }
+
+  function nextBtn() {
+    setCurentImg(currentImg > images.length - 2 ? 0 : currentImg + 1);
+  }
+
   return (
     <div>
       <h1>Carousel</h1>
-      <button className="left">&larr;</button>
-      <div className="imgContainer">
-      <img src={images[3]} alt="nature"/>
-      </div>
-      <button className="right">&rarr;</button>
-    </div>
-  )
-}
+      <button className="left" onClick={prevBtn}>
+        &larr;
+      </button>
 
+      {images.map(
+        (img, i) =>
+          currentImg === i && (
+            <div className="imgContainer" key={img}>
+              <img src={img} alt="nature-img" />
+            </div>
+          )
+      )}
+
+      <button className="right" onClick={nextBtn}>
+        &rarr;
+      </button>
+    </div>
+  );
+}
